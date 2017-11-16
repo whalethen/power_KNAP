e0fd98bd9ad863482d947eaf33dc7f91bbd8594f
Phoebe <meipbe@gmail.com>
2017-11-15 22:32:56 -0800

    Added functions to fetch video data from Youtube API

--- server/fetchYouTubeVideo.js
+++ server/fetchYouTubeVideo.js
@@ -1,20 +1,38 @@
 const rp = require('request-promise');
-const config = require('../config');
+const config = require('../config.js');
+const isoConvert = require('convert-iso8601-duration');
 
 const fetchYoutubeVideo = (query) => {
-  let params = {
+  const options = {
     url: 'https://www.googleapis.com/youtube/v3/search',
-    method: 'GET',
     qs: {
-      key: config.YT_API_KEY,
+      key: config.config.YT_API_KEY,
       q: query,
       type: 'video',
       part: 'snippet',
     },
+    json: true,
   };
-  rp(params)
+  rp(options)
     .then(results => console.log('success: ', results))
     .catch(err => console.error('Could have retrieve Youtube search results: ', err));
 };
 
-fetchYoutubeVideo('cats');
+// fetchYoutubeVideo('cats');
+
+const fetchVideoLength = (videoId) => {
+  const options = {
+    url: 'https://www.googleapis.com/youtube/v3/videos',
+    qs: {
+      key: config.config.YT_API_KEY,
+      part: 'contentDetails',
+      id: videoId,
+    },
+    json: true,
+  };
+  rp(options)
+    .then(results => console.log('success: ', isoConvert(results.items[0].contentDetails.duration)))
+    .catch(err => console.error('Could have retrieve Youtube search results: ', err));
+};
+
+// fetchVideoLength('7lCDEYXw3mM');
