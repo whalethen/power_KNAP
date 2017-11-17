f1fb0289d46b6d92e8a4d525265c13f43e9d7507
Phoebe <meipbe@gmail.com>
2017-11-16 20:34:35 -0800

    Websocket should now retrieve video data from client when user adds new video to playlist

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
-const fetch = require('./fetchYouTubeVideo');
+const youtubeApi = require('./fetchYouTubeVideo');
+const db = require('../database/postgres');
 
 const app = express();
 const port = 8080;
@@ -12,13 +13,27 @@ server.listen(port, () => console.log(`listening on port ${port}`));
 app.use(express.static(`${__dirname}./../client`));
 
 io.on('connection', (socket) => {
-  console.log(`connected to client`);
+  console.log('connected to client');
 
   // listen for incoming youtube searches
   socket.on('youtubeSearch', (query) => {
-    fetch.youtubeVideos(query)
+    youtubeApi.fetchYoutubeVideos(query)
       .then(videos => io.emit('searchResults', videos))
       .catch(err => io.emit('error', err));
+  });
 
+  socket.on('saveVideoToPlaylist', (video) => {
+    return youtubeApi.fetchVideoLength(video.id.videoId)
+      .then((videoDuration) => {
+        const videoData = {
+          title: video.snippet.title,
+          creator: video.snippet.channelTitle,
+          url: `https://www.youtube.com/embed/${video.id.videoId}?autoplay=1`,
+          duration: videoDuration,
+        };
+        return db.storeVideoInDatabase(videoData);
+      })
+      .then(result => console.log(result))
+      .catch(err => console.error('Could not retrieve YT data: ', err));
   });
 });
