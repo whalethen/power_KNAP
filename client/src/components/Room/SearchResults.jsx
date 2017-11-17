84c91f232e9d14a54e543e834edd5fe920a7f224
Kory <koryrkilpatrick@gmail.com>
2017-11-16 20:20:35 -0800

    added plusbutton click functionality client-side

--- client/src/components/Room/SearchResults.jsx
+++ client/src/components/Room/SearchResults.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchResultsEntry from './SearchResultsEntry';
 
-const SearchResults = ({ searchResults }) => (
+const SearchResults = ({ searchResults, saveToPlaylist }) => (
   <div>
     {searchResults.map(searchResult =>
-      <SearchResultsEntry searchResult={searchResult} key={searchResult.etag} />)}
+      <SearchResultsEntry searchResult={searchResult} key={searchResult.etag} saveToPlaylist={saveToPlaylist} />)}
   </div>
 );
 
 SearchResults.propTypes = {
   searchResults: PropTypes.arrayOf(PropTypes.object).isRequired,
+  saveToPlaylist: PropTypes.func.isRequired,
 };
 
 export default SearchResults;
