1c353f140fda0af61bf9d9eddd1d8068f7a74cea
Kory <koryrkilpatrick@gmail.com>
2017-11-18 19:57:45 -0800

    commit in case i rekt things

--- database/postgres.js
+++ database/postgres.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://onnalrnmsdaawl:d120ca2df7e240ca6ba016ea88eec78ce24d87f5348e5a5ed544f4fda86158cd@ec2-54-235-168-152.compute-1.amazonaws.com:5432/d500oa7t03a9qh', { dialect: 'postgres', protocol: 'postgres', dialectOptions: { ssl: true } });
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://korykilpatrick:password@localhost:5432', { dialect: 'postgres', protocol: 'postgres', dialectOptions: { ssl: true } });
 
 // const sequelize = new Sequelize('famly', 'Phoebe', 'password', {
 //   host: 'postgres://onnalrnmsdaawl:d120ca2df7e240ca6ba016ea88eec78ce24d87f5348e5a5ed544f4fda86158cd@ec2-54-235-168-152.compute-1.amazonaws.com:5432/d500oa7t03a9qh',
 //   dialect: 'postgres',
 // });
+
 
 sequelize.authenticate()
   .then(() => {
