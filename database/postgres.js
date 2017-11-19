445000b9a3763818578a45d6b01a683347ace9a3
Kory <koryrkilpatrick@gmail.com>
2017-11-18 20:28:47 -0800

    added env params

--- database/postgres.js
+++ database/postgres.js
@@ -1,11 +1,7 @@
+const env = require('../.env');
+
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://korykilpatrick:password@localhost:5432', { dialect: 'postgres', protocol: 'postgres', dialectOptions: { ssl: true } });
-
-// const sequelize = new Sequelize('famly', 'Phoebe', 'password', {
-//   host: 'postgres://onnalrnmsdaawl:d120ca2df7e240ca6ba016ea88eec78ce24d87f5348e5a5ed544f4fda86158cd@ec2-54-235-168-152.compute-1.amazonaws.com:5432/d500oa7t03a9qh',
-//   dialect: 'postgres',
-// });
-
+const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres', protocol: 'postgres', dialectOptions: { ssl: true } });
 
 sequelize.authenticate()
   .then(() => {
