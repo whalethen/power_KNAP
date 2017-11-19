989341c4183735dfcf7d6fb3d112fd52c70a87ae
Phoebe <meipbe@gmail.com>
2017-11-18 16:21:26 -0800

    Changed file name from fetchYouTubeVideos to current

---
+++ server/youtubeService.js
@@ -0,0 +1,31 @@
+const rp = require('request-promise');
+const config = require('../config.js');
+const isoConvert = require('convert-iso8601-duration');
+
+exports.grabVideos = (query) => {
+  const options = {
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    qs: {
+      key: config.config.YT_API_KEY,
+      q: query,
+      type: 'video',
+      part: 'snippet',
+    },
+    json: true,
+  };
+  return rp(options);
+};
+
+exports.grabVideoLength = (videoId) => {
+  const options = {
+    url: 'https://www.googleapis.com/youtube/v3/videos',
+    qs: {
+      key: config.config.YT_API_KEY,
+      part: 'contentDetails',
+      id: videoId,
+    },
+    json: true,
+  };
+  return rp(options)
+    .then(results => isoConvert(results.items[0].contentDetails.duration));
+};
