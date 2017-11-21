192fb8e584517b2aa3902a733eba08b3ab825234
Kory <koryrkilpatrick@gmail.com>
2017-11-20 22:48:55 -0800

    env testing

--- database/postgres.js
+++ database/postgres.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
 
-let params = {};
-if (!process.env.LOCAL) { params = { dialect: 'postgres', protocol: 'postgres', dialectOptions: { ssl: true } }; }
-const sequelize = new Sequelize(process.env.DATABASE_URL, params);
+
+// let params = {};
+// if (!process.env.LOCAL) { params = { dialect: 'postgres', protocol: 'postgres', logging: false, dialectOptions: { ssl: true } }; }
+const sequelize = new Sequelize(match[5], match[1], match[2], {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging: false,
+    dialectOptions: {
+        ssl: true
+    };
+});
 
 sequelize.authenticate()
   .then(() => console.log('Connection has been established successfully'))
