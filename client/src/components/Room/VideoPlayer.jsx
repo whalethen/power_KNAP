8fcdebb32368134b01be8d06a28610b5bfaa4713
Kory <koryrkilpatrick@gmail.com>
2017-11-22 11:26:47 -0800

    remove need for sample data

--- client/src/components/Room/VideoPlayer.jsx
+++ client/src/components/Room/VideoPlayer.jsx
@@ -2,24 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import YouTube from 'react-youtube';
 
-const VideoPlayer = ({ currentVideo, onReady, onStateChange }) => (
-  <div className="container videoplayer">
-    <YouTube
-      // className="videoFrame" // TODO uncomment to restrict player controls
-      videoId={currentVideo.url}
-      onReady={onReady}
-      onStateChange={onStateChange}
-    />
-    <div className="currentSongDetails">
-      <div className="videoTitle"> {currentVideo.videoName} </div>
-      <div className="songDescription"> {currentVideo.description} </div>
+const VideoPlayer = ({ currentVideo, onReady, onStateChange }) => {
+  if (currentVideo !== undefined) {
+    return (
+      <div className="container videoplayer">
+        <YouTube
+          // className="videoFrame" // TODO uncomment to restrict player controls
+          videoId={currentVideo.url}
+          onReady={onReady}
+          onStateChange={onStateChange}
+        />
+        <div className="currentSongDetails">
+          <div className="videoTitle"> {currentVideo.videoName} </div>
+          <div className="songDescription"> {currentVideo.description} </div>
+        </div>
+        <div className="line" />
+      </div>
+    );
+  }
+  return (
+    <div className="container videoplayer">
+      <img src="https://www.reaconverter.com/howto/wp-content/uploads/2015/10/banana.gif" alt="pizza boy" />
     </div>
-    <div className="line" />
-  </div>
-);
+  );
+};
+
 
 VideoPlayer.propTypes = {
-  currentVideo: PropTypes.instanceOf(Object).isRequired,
+  currentVideo: PropTypes.instanceOf(Object),
   onStateChange: PropTypes.func.isRequired,
   onReady: PropTypes.func.isRequired,
 };
