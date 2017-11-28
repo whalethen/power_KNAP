5a2adeabf5e51e1b572e43d9f97fec96c3c9f4bd
Andrew Nguyen <andy.andino93@gmail.com>
2017-11-28 15:57:25 -0800

    clean up per style guide

--- server/auth-routes.js
+++ server/auth-routes.js
@@ -3,15 +3,15 @@ const passport = require('passport');
 
 router.get('/logout', (req, res) => {
   req.logout();
-  res.clearCookie("user").redirect('/') 
+  res.clearCookie('user').redirect('/');
 });
 
 router.get('/google', passport.authenticate('google', {
-  scope:['profile'] // put other things in here you may want from google (check API)
+  scope: ['profile'], // put other things in here you may want from google (check API)
 }));
 
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-  res.cookie('user', req.user.displayName).redirect('/')
+  res.cookie('user', req.user.displayName).redirect('/');
 });
 
 module.exports = router;
