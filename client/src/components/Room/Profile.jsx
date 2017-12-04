6b886555af570422c2630eb668b02f6a2dae1c53
Angela Huang <angelahuang89@gmail.com>
2017-12-03 16:59:41 -0800

    Pass info to Profile and render with styles

--- client/src/components/Room/Profile.jsx
+++ client/src/components/Room/Profile.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Profile = props => (
-  <div>Hello</div>
-);
+const Profile = (props) => {
+  const { user } = props.location.state;
+  return (
+    <div className="profile">
+      <h1>Profile</h1>
+      <div>{user}</div>
+    </div>
+  );
+};
 
-// Profile.propTypes = {
-
-// };
+Profile.propTypes = {
+  // user: PropTypes.string.isRequired,
+  // location: PropTypes.instanceOf(object).isRequired,
+};
 
 export default Profile;
